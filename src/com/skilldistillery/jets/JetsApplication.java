e6c55fc61fb77c7c0d90fc27c21aaa767349af77
JimmyTran <quantrannhu@gmail.com>
2023-12-01 14:54:48 -0800

    initial commit

---
+++ src/com/skilldistillery/jets/JetsApplication.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.jets;
+
+import java.util.Scanner;
+
+public class JetsApplication {
+
+	private AirField airField;
+	private Scanner kb=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
