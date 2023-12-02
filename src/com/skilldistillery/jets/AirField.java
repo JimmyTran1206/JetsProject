a8ab80dd375a219a2be3035187fbda7739a70873
JimmyTran <quantrannhu@gmail.com>
2023-12-01 16:34:14 -0800

    Finish User Story 1-3

--- src/com/skilldistillery/jets/AirField.java
+++ src/com/skilldistillery/jets/AirField.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 public class AirField {
 	private ArrayList<Jet> jets;
 	public AirField () {
-		super();
+		jets.add(new CargoPlane("AirBus A300", 544, 4000, 105_000_000)); //data according to Google
+		jets.add(new CargoPlane("Lockheed L-100 Hercules", 355, 1535, 4_800_000)); // data according to Google
+		jets.add(new FighterJet("F-35C", 1200, 1400, 75_000_000)); //data according to Google
+		jets.add(new FighterJet("F-18F", 1190, 2069, 13_700_000)); //data according to Google
+		jets.add(new PassengerJet("Boeing 747", 614, 8358, 418_000_000)); //data according to Google		
 	}
 
 }
